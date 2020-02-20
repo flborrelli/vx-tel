2fc4bc829680448490906c33c858c7df41ac3f34
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-02-20 13:02:09 -0300

    Setting plan calculation fees

--- src/App.js
+++ src/App.js
@@ -17,26 +17,52 @@ function App() {
   const [valueWithoutPlan, setValueWithoutPlan] = useState(0);
 
   useEffect(() => {
-    setValues(values); 
-    setValueWithPlan(valueWithPlan); 
-    setValueWithoutPlan(valueWithoutPlan)
+    setValues(values);
+    setValueWithPlan(valueWithPlan);
+    setValueWithoutPlan(valueWithoutPlan);
   }, [values, valueWithPlan, valueWithoutPlan]);
+
+  const elevenToSixteen = values.minutes * 1.9;
+  const elevenToSeventeen = values.minutes * 1.7;
+  const elevenToEighteen = values.minutes * 0.9;
+  const seventeenToEleven = values.minutes * 2.9;
+  const seventeenToEleven = values.minutes * 2.7;
+  const eighteenToEleven = values.minutes * 1.9;
+
+
 
   const callPriceCalculation = () => {
     switch (values.origin) {
       case "011":
         if (values.destination === "016") {
-          setValueWithoutPlan(values.minutes * 1.9);
+          setValueWithoutPlan(elevenToSixteen);
         } else if (values.destination === "017") {
-          setValueWithoutPlan(values.minutes * 1.7);
+          setValueWithoutPlan(elevenToSeventeen);
         } else if (values.destination === "018") {
-          setValueWithoutPlan(values.minutes * 0.9);
+          setValueWithoutPlan(elevenToEighteen);
         }
         break;
       case "016":
-        setValueWithoutPlan(values.minutes * 2.9);
+        setValueWithoutPlan(seventeenToEleven);
         break;
       case "017":
+        setValueWithoutPlan(seventeenToEleven);
+        break;
+      case "018":
+        setValueWithoutPlan(eighteenToEleven);
+        break;
+    }
+  };
+
+  const planCalculation = initialValue => {
+    switch (values.plan) {
+      case "Plano FaleMais 30":
+        setValueWithPlan()
+        break;
+      case "Plano FaleMais 60":
+        setValueWithoutPlan(values.minutes * 2.9);
+        break;
+      case "Plano FaleMais 120":
         setValueWithoutPlan(values.minutes * 2.7);
         break;
       case "018":
@@ -47,16 +73,15 @@ function App() {
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log('submiting')
+    console.log("submiting");
     callPriceCalculation();
   };
 
   const handleInputChange = (e, result) => {
     const { name, value } = result || e.target.value;
     setValues({ ...values, [name]: value });
-    console.log(values)
+    console.log(values);
   };
-
 
   return (
     <>
